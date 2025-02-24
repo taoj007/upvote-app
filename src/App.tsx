3d2b1beeb38ab24ab2968c804e47060e880b1817
Tao Jiang <joint.007@163.com>
2025-02-24 17:01:30 -0500

    add noopner

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
     return (
         <AppWrapper>
             <h1>Upvote List</h1>
-            <p>Please refer to <a href="https://github.com/taoj007/upvote-app/tree/main" target='_blank'>Github repo Link</a></p>
+            <p>Please refer to <a href="https://github.com/taoj007/upvote-app/tree/main" target='_blank' rel="noopener noreferrer">Github repo Link</a></p>
             <UpvoteList/>
         </AppWrapper>
     );
